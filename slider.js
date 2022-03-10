1495fcff67b2ee710855491518e299980fe725f5
rodrigo riveros <riverosrodrigo@081.com>
2022-03-09 21:50:57 -0300

    subo cambios

--- slider.js
+++ slider.js
@@ -1,46 +1,49 @@
-//obtengo slider
-const slider=document.querySelector("#slider");
-let sliderSection=document.querySelectorAll(".slider__section");
-let sliderSectionLast=sliderSection[sliderSection.length -1];
-
-const bntLeft=document.querySelector("#btn-left");
-const bntRight=document.querySelector("#btn-right");
-
-// la ultima imagen la paso al principio
-slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-
-//ejecuto funcion next (movimiento imagenes)
-function Next(){
-    let sliderSectionFirst= document.querySelectorAll(".slider__section") [0];
-    slider.style.marginLeft= "-200%";
-    slider.style.transition= "all 0.5s";
-    setTimeout(function(){
-        slider.style.transition="none";
-        slider.insertAdjacentElement('beforeend', sliderSectionFirst);
-        slider.style.marginLeft="-100%";
-    },500);
-}
-//ejecuto funcion prev (movimiento imagenes)
-function Prev(){
-    let sliderSection= document.querySelectorAll(".slider__section");
-    let sliderSectionLast=sliderSection[sliderSection.length -1];
-    slider.style.marginLeft="0";
-    slider.style.transition="all 0.5s";
-    setTimeout(function(){
-        slider.style.transition="none";
-        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-        slider.style.marginLeft="-100%";
-    },500);
-}
-
-bntRight.addEventListener('click',function(){
-    Next();
+let btnMenu = document.getElementById('btn-menu');
+let mainNav = document.getElementById('main-nav');
+btnMenu.addEventListener('click', function(){
+  mainNav.classList.toggle('mostrar');
 });
 
-bntLeft.addEventListener('click', function(){
-    Prev();
-  });
-  
-  setInterval(function(){
-    Next();
-  }, 5000);
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+  let sliderSectionFirst = document.querySelectorAll(".slider__section")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('beforeend', sliderSectionFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function Prev() {
+  let sliderSection = document.querySelectorAll(".slider__section");
+  let sliderSectionLast = sliderSection[sliderSection.length -1];
+  slider.style.marginLeft = "0";
+  slider.style.transition = "all 0.5s";
+  setTimeout(function(){
+    slider.style.transition = "none";
+    slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+  Next();
+});
+
+btnLeft.addEventListener('click', function(){
+  Prev();
+});
+
+setInterval(function(){
+  Next();
+}, 5000);
